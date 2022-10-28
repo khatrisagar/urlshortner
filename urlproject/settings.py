72835a553019660217a50492333c67f18209be3b
khatrisagar <khatrisagar404@gmail.com>
2022-10-28 20:46:32 +0530

    db added

--- urlproject/settings.py
+++ urlproject/settings.py
@@ -24,7 +24,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dp*^4azyazj+huvw8+&7sj(*#_q@h12f-q_0np^4p@u=#t)da!'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
